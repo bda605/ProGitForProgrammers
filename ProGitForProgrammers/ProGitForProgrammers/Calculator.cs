d01b31773b47ddefc2787b8909e270ba4c4844c2
Eddie_Chen <bda605@hotmail.com>
2023-10-22 00:31:15 +0800

    add add method

--- ProGitForProgrammers/ProGitForProgrammers/Calculator.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Calculator.cs
@@ -8,5 +8,10 @@ namespace ProGitForProgrammers
 {
     internal class Calculator
     {
+        public int Add(int left, int right)
+        {
+            return left + right;
+        }
+
     }
 }
